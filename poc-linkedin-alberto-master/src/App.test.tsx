7376a3278b14a170a9769af75363859ffd2a212c
Alberto Vinícius A. dos Santos <89992304+allbertuu@users.noreply.github.com>
2022-07-18 11:37:31 -0300

    Project's setup finished

---
+++ poc-linkedin-alberto-master/src/App.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renderizar componente Button com texto', () => {
+  render(<App />);
+  const ButtonText = screen.getByText('Primeiro componente')
+  expect(ButtonText).toBeInTheDocument();
+});
---
+++ poc-linkedin-alberto-master/src/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { GlobalStyle } from "./styles/GlobalStyle";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <GlobalStyle />
+    <App />
+  </React.StrictMode>
+);
