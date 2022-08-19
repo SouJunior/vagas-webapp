e01a838f5c18fc43b9dce99bfbe741d2376bdc50
silvia_teixeira <quimicasilvia20@gmail.com>
2022-08-19 13:11:13 -0300

    feat: Delete routes.tsx and implement routes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import InsertJobs from "./pages/InsertJobs";
+import Login from "./pages/Login";
 
 
 function App() {
-
   return (
-    <></>
+      <Routes>
+          <Route path="/" element={<Login/>} />
+          <Route path="/InsertJobs" element={<InsertJobs/>} />
+      </Routes>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Router from "./routes";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { GlobalStyle } from "./styles/GlobalStyle";
