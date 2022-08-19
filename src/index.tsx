0e26755a395add45436526f5944842fcc06cdf69
silvia_teixeira <quimicasilvia20@gmail.com>
2022-08-19 13:13:35 -0300

    Fix: Delete import BrowserRouter from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import "./index.css";
