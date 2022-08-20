3b9c4dbc88145a9823fb2afe1b61fe70bba2861a
ArthurMaciel95 <arthurnmrocha@gmail.com>
2022-08-20 10:53:19 -0300

    feat: criando helpers para candidatos

---
+++ src/helpers/candidates.js
@@ -0,0 +1,67 @@
+// eslint-disable-next-line no-unused-expressions
+export const candidates = [
+    {
+        avatar: require("../assets/image/user1.png"),
+        links: ["Linkedin", "Behance", "Email"],
+        email: "arthurnmrocha@gmail.com",
+        name: "Name",
+        location: "Office",
+    },
+    {
+        avatar: require("../assets/image/user2.png"),
+        links: ["Linkedin", "Behance", "Email"],
+        email: "arthurnmrocha@gmail.com",
+        name: "Name",
+        location: "Office",
+    },
+    {
+        avatar: require("../assets/image/user3.png"),
+        links: ["Linkedin", "Behance", "Email"],
+        email: "arthurnmrocha@gmail.com",
+        name: "Name",
+        location: "Office",
+    },
+    {
+        avatar: require("../assets/image/user4.png"),
+        links: ["Linkedin", "Behance", "Email"],
+        email: "arthurnmrocha@gmail.com",
+        name: "Name",
+        location: "Office",
+    },
+    {
+        avatar: require("../assets/image/user5.png"),
+        links: ["Linkedin", "Behance", "Email"],
+        email: "arthurnmrocha@gmail.com",
+        name: "Name",
+        location: "Office",
+    },
+    {
+        avatar: require("../assets/image/user6.png"),
+        links: ["Linkedin", "Behance", "Email"],
+        email: "arthurnmrocha@gmail.com",
+        name: "Name",
+        location: "Office",
+    },
+    {
+        avatar: require("../assets/image/user7.png"),
+        links: ["Linkedin", "Behance", "Email"],
+        email: "arthurnmrocha@gmail.com",
+        name: "Name",
+        location: "Office",
+    },
+    {
+        avatar: require("../assets/image/user8.png"),
+        links: ["Linkedin", "Behance", "Email"],
+        email: "arthurnmrocha@gmail.com",
+        name: "Name",
+        location: "Office",
+    },
+    {
+        avatar: require("../assets/image/user9.png"),
+        links: ["Linkedin", "Behance", "Email"],
+        email: "arthurnmrocha@gmail.com",
+        name: "Name",
+        location: "Office",
+    },
+];
+
