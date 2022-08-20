a75b2ef9cc7803a202357d7f29da1269d40dffad
ArthurMaciel95 <arthurnmrocha@gmail.com>
2022-08-20 10:51:39 -0300

    feat: adicionando nova rota home

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "./pages/Login"
+import Login from "./pages/Login";
 import App from "./App";
 import Home from "./pages/Home";
 
-
 const Router = () => {
-    return(
+    return (
         <Routes>
-            <Route path="/" element={<Login/>} />
-            <Route path="/job" element={<App/>} />
-            <Route path="/home" element={<Home />}/>
+            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/job" element={<App />} />
         </Routes>
     );
 };
