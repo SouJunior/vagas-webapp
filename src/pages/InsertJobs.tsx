2871fa45d4e9f8ea5e2a86ed2b0d2be3cc6a2d34
Alberto <albertovinicius3@gmail.com>
2022-08-28 11:12:13 -0300

    setup insertJobs.tsx

--- src/pages/InsertJobs.tsx
+++ src/pages/InsertJobs.tsx
@@ -81,88 +81,84 @@ function InsertJobs() {
   }
 
   return (
-    <div className="App-wrapper">
-      <header className="p-5 text-2xl">SouJunior</header>
-      <hr />
-      <main className="grid grid-cols-1 md:grid-cols-5 py-4">
-        <form
-          onSubmit={(event) => handleFormSubmit(event)}
-          className="m-4 p-4 border h-min col-span-2"
-          id="form"
-        >
-          <h1 className="font-bold text-lg">Preencha os campos</h1>
-          <div className="py-2">
-            <label htmlFor="title">Titulo da vaga</label>
-            <input
-              type="text"
-              name="title"
-              id="title"
-              onChange={(event) => handleFieldChange(event)}
-              className="w-full border-2 py-1 px-3 block outline-none focus:border-blue-600"
-            />
-          </div>
-
-          <div className="py-2">
-            <label htmlFor="description">Descrição da vaga</label>
-            <textarea
-              name="description"
-              id="description"
-              onChange={(event) => handleFieldChange(event)}
-              className="w-full h-40 border-2 py-1 px-3 block outline-none focus:border-blue-600"
-            />
-          </div>
-
-          <div className="py-2">
-            <label htmlFor="jobType">Tipo da vaga</label>
-            <select
-              name="jobType"
-              id="jobType"
-              onChange={(event) => handleFieldChange(event)}
-              required
-              defaultValue={"- Selecione aqui -"}
-              className="w-full border-2 py-1 px-3 block outline-none focus:border-blue-600"
-            >
-              <option disabled>- Selecione aqui -</option>
-              <option value="Estágio">Estágio</option>
-              <option value="Júnior">Júnior</option>
-              <option value="Trainee">Trainee</option>
-            </select>
-          </div>
-          <div className="flex gap-2">
-            <button
-              type="submit"
-              onClick={(event) => handleFormSubmit(event)}
-              className="w-full inline-block rounded font-semibold bg-yellow-500 px-2 py-1 hover:bg-yellow-600 active:bg-yellow-500 focus:ring focus:ring-yellow-300 text-white"
-            >
-              Cadastrar
-            </button>
-            <button
-              type="reset"
-              className="w-fit text-sm inline-block rounded font-semibold bg-slate-500 px-2 py-1 hover:bg-slate-600 active:bg-slate-500 focus:ring focus:ring-slate-300 text-white"
-            >
-              Limpar
-            </button>
-          </div>
-        </form>
-
-        <div className="m-4 col-span-3 h-96 overflow-scroll border-b-4">
-          {jobsList.length > 0 ? (
-            jobsList.map((job) => (
-              <JobCard
-                key={job.id}
-                id={job.id}
-                title={job.title}
-                description={job.description}
-                jobType={job.jobType}
-                deleteJobCard={() => deleteJobCard(job.id)}
-              />
-            ))
-          ) : (
-            <b>Não há vagas disponíveis :(</b>
-          )}
+    <main className="grid grid-cols-1 md:grid-cols-5 py-4">
+      <form
+        onSubmit={(event) => handleFormSubmit(event)}
+        className="m-4 p-4 border h-min col-span-2"
+        id="form"
+      >
+        <h1 className="font-bold text-lg">Preencha os campos</h1>
+        <div className="py-2">
+          <label htmlFor="title">Titulo da vaga</label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            onChange={(event) => handleFieldChange(event)}
+            className="w-full border-2 py-1 px-3 block outline-none focus:border-blue-600"
+          />
         </div>
-      </main>
-    </div>
+
+        <div className="py-2">
+          <label htmlFor="description">Descrição da vaga</label>
+          <textarea
+            name="description"
+            id="description"
+            onChange={(event) => handleFieldChange(event)}
+            className="w-full h-40 border-2 py-1 px-3 block outline-none focus:border-blue-600"
+          />
+        </div>
+
+        <div className="py-2">
+          <label htmlFor="jobType">Tipo da vaga</label>
+          <select
+            name="jobType"
+            id="jobType"
+            onChange={(event) => handleFieldChange(event)}
+            required
+            defaultValue={"- Selecione aqui -"}
+            className="w-full border-2 py-1 px-3 block outline-none focus:border-blue-600"
+          >
+            <option disabled>- Selecione aqui -</option>
+            <option value="Estágio">Estágio</option>
+            <option value="Júnior">Júnior</option>
+            <option value="Trainee">Trainee</option>
+          </select>
+        </div>
+        <div className="flex gap-2">
+          <button
+            type="submit"
+            onClick={(event) => handleFormSubmit(event)}
+            className="w-full inline-block rounded font-semibold bg-yellow-500 px-2 py-1 hover:bg-yellow-600 active:bg-yellow-500 focus:ring focus:ring-yellow-300 text-white"
+          >
+            Cadastrar
+          </button>
+          <button
+            type="reset"
+            className="w-fit text-sm inline-block rounded font-semibold bg-slate-500 px-2 py-1 hover:bg-slate-600 active:bg-slate-500 focus:ring focus:ring-slate-300 text-white"
+          >
+            Limpar
+          </button>
+        </div>
+      </form>
+
+      <div className="m-4 col-span-3 h-96 overflow-scroll border-b-4">
+        {jobsList.length > 0 ? (
+          jobsList.map((job) => (
+            <JobCard
+              key={job.id}
+              id={job.id}
+              title={job.title}
+              description={job.description}
+              jobType={job.jobType}
+              deleteJobCard={() => deleteJobCard(job.id)}
+            />
+          ))
+        ) : (
+          <b>Sem vagas registradas até o momento :(</b>
+        )}
+      </div>
+    </main>
   );
 }
 
