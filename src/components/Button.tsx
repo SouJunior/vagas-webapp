d7a266256cc80b7b179f75f8a71ef0031bb9cac5
silvia_teixeira <quimicasilvia20@gmail.com>
2022-08-31 15:16:14 -0300

    feat : button styled changes adding outline button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,10 @@
+import React from "react"
 import { ButtonWrapper } from "./styles/Button.styled"
 
-export const Button = () => {
+export const Button : React.FC<any> = ({background, children}) => {
   return(
-    <ButtonWrapper>
-      Primeiro botão
+    <ButtonWrapper background={background}>
+      {children}
     </ButtonWrapper>
   )
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,7 @@
 /* aqui estara a pagina de login */
 
 import React from "react";
+import { Button } from "../components/Button";
 
 const Login = () => {
     return (
