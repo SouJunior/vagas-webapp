8c64f160ddbf36ea8cbef74a305027a41114bbca
Silvia Luiza <84419782+SilviaLTeixeira@users.noreply.github.com>
2022-09-06 15:34:55 -0300

    Update src/components/Button.tsx
    Co-authored-by: Mateus Santana <nesantana15@gmail.com>

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { ButtonWrapper } from "./styles/Button.styled"
 
-export const Button : React.FC<any> = ({background,border, children}) => {
+export const Button : React.FC<any> = ({background, border, children}) => {
   return(
     <ButtonWrapper background={background} border={border}>
       {children}
