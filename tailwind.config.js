35c70c7625d8d9e48caa5dec89945e73086eea5f
Alberto <albertovinicius3@gmail.com>
2022-09-14 22:45:06 -0300

    chore(tailwind.config): add customized colors and gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          lighter: "#ECF5FF",
+          light: "#086BCF",
+          DEFAULT: "#1165BA",
+          dark: "#014C97",
+        },
+        gray: {
+          light: "#D8D8D8",
+          DEFAULT: "#F3F3F3",
+          dark: "#C1C1C1",
+          darker: "#515050",
+        },
+      },
+      backgroundImage: {
+        "blue-gradient":
+          "linear-gradient(294.77deg, #0A67C7 50.32%, rgba(0, 114, 231, 0.67) 100%)",
+      },
+    },
   },
   plugins: [],
-}
+};
