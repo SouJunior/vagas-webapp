6248bf39213c356542ce0ff48b87bea8a11fbd11
Alberto <albertovinicius3@gmail.com>
2022-09-15 17:24:45 -0300

    style: format with prettier
    Co-authored-by: Silvia Luiza <SilviaLTeixeira@users.noreply.github.com>

--- src/components/styles/Button.styled.ts
+++ src/components/styles/Button.styled.ts
@@ -1,44 +1,43 @@
-import styled, {css} from "styled-components";
+import styled, { css } from "styled-components";
+
+// TODO: ajustar componente styled para receber attrs próprios de um botão HTML
 
 interface ButtonWrapperProps {
-    background?: "outline";
-    border?: "white";
+  background?: "outline";
+  border?: "white";
 }
-export const ButtonWrapper = styled.button<ButtonWrapperProps>`
+
+export const ButtonWrapper = styled.button<ButtonWrapperProps | any>`
   padding: 8px 12px;
-  background-color: ${({theme}) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.primary};
   border: 1px solid #004892;
   border-radius: 4px;
-  color: #FFF;
+  color: #fff;
 
   min-width: fit-content;
-
-  box-sizing: boder-box;
-  
-  font-size: 14px;
 
   display: flex;
   align-items: center;
   justify-content: center;
-  
+
   cursor: pointer;
   outline: none;
 
   &:active {
-    background-color: ${({theme}) => theme.colors.primaryDark};
+    background-color: ${({ theme }) => theme.colors.primaryDark};
   }
 
-  ${({background}) => background === "outline" && css`
-    background: transparent;
-    border-color: ${({theme}) => theme.colors.primary};
-    color: ${({theme}) => theme.colors.primary};
-    
-  `}
-  ${({border}) => border === "white" && css`
-    border-color: ${({theme}) => theme.colors.muted};
-    color: ${({theme}) => theme.colors.muted};
-  `}
-
-
-
+  ${({ background }) =>
+    background === "outline" &&
+    css`
+      background: transparent;
+      border-color: ${({ theme }) => theme.colors.primary};
+      color: ${({ theme }) => theme.colors.primary};
+    `}
+  ${({ border }) =>
+    border === "white" &&
+    css`
+      border-color: ${({ theme }) => theme.colors.muted};
+      color: ${({ theme }) => theme.colors.muted};
+    `}
 `;
