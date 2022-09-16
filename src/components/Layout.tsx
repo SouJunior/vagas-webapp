54676247b96d8b00b4073190b5709b598b776ada
Alberto <albertovinicius3@gmail.com>
2022-09-16 09:30:46 -0300

    refactor(Layout.tsx): remove header component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
-import Header from "./Header";
 import { Container } from "./styles/Container.styled";
 
 export default function Layout() {
   return (
     <>
-      <Header />
       <Container>
         <Outlet />
       </Container>
