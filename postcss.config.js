4080216b45b034e032fd3c3016d949f36cc8d424
CarlosJunioor <carlosjunior866@gmail.com>
2022-09-20 23:04:04 +0100

    feat:tailwind configs

--- postcss.config.js
+++ postcss.config.js
@@ -4,3 +4,13 @@ module.exports = {
     autoprefixer: {},
   },
 }
+
+module.exports = {
+  theme: {
+    extend: {
+      spacing: {
+        '128': '32rem',
+      }
+    }
+  }
+}
--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -13,8 +13,8 @@ const Report = () => {
                 <h2>Após denuncias a vaga nossa equipe ira tomar as devidas providëncias em relação a denúncia.</h2>
                 <hr className="mb-7" />
                 <Outlet />
-            <div className='grid grid-cols-2 gap-2'>
-                <div>
+            <div className='grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-8'>
+                <div className='w-96 h-96 '>
                 Denunciar esta Vaga
 
                 UX Designer Junior
@@ -38,8 +38,8 @@ const Report = () => {
                 
                 Responsabilidades e atribuições: 
                 </div>
-                    <div>
-                    Por que está denunciando esta vaga?
+                    <div className='m-4 p-4 rounded-md border h-min col-span-2'>
+                    Por que está denunciando esta vaga?<br></br>
                     Informe abaixo a informação mais relevante para a denuncia da vaga!
                 <hr></hr>
                 <br></br>
