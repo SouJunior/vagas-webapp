bc0487104fc083f6b3902fff88b39bb2d9bc326d
Alberto <albertovinicius3@gmail.com>
2022-09-21 17:10:08 -0300

    fix(Layout.tsx): remove unnecessary imports

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,4 @@
 import { Outlet } from "react-router-dom";
-import Header from "./Header";
-import Footer from "./Footer";
 import { Container } from "./styles/Container.styled";
 
 export default function Layout() {
