079ae497f431b65bfb99b688d2891bc02d947f70
CarlosJunioor <carlosjunior866@gmail.com>
2022-09-26 16:34:23 +0100

    feat: report-card is done

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -10,25 +10,25 @@ const Report = () => {
             <Header/>
             <Container>
                 <h1>Informe a equipe do SouJunior o problema desta vaga</h1>
-                <h2 className='border-b'>Após denuncias a vaga nossa equipe ira tomar as devidas providëncias em relação a denúncia.</h2>
+                <h2 className='border-b '>Após denuncias a vaga nossa equipe ira tomar as devidas providëncias em relação a denúncia.</h2>
                 <br></br>
                 <Outlet />
             <div className='grid grid-cols-2 md:grid-cols-2 lg:grid-cols-2'>
-                <div className='p-0 bg-white border rounded max-w-sm'>
-                <div className='p-1 bg-red-600 rounded text-white'>Denunciar esta Vaga</div>
+                <div className='p-1 bg-white w-[583px] h-[560px] rounded-[11px] border-2 border-red-600 flex-nowrap overflow-auto' >
+                <p className='p-1 ring-4 ring-red-600 rounded bg-red-600 text-white'>Denunciar esta Vaga</p>
                 
                 <div className='p-1 rounded-t-lg '>
                 <section className='p-4'>
-                <h3>UX Designer Junior</h3>
-                <p>Empresa Verde</p>
+                <h3 className='font-extrabold text-[26px]'>UX Designer Junior</h3>
+                <p className='text-[16px] font-semibold'>Empresa Verde</p>
                 <p>Bento Gonçalves, Rio Grande do Sul, Brasil</p>
                 <br></br>
-                <p className='p-4'>REMOTO - JUNIOR - PJ</p>
+                <p className='p-4 font-semibold'>REMOTO - JUNIOR - PJ</p>
 
                 <div className='p-4 box-border border-2 rounded-md'>
                 <p>Tempo Integral</p>
                 <p>51-200 funcionários</p>
-                <p>Recrutando agora</p>
+                <a href="#" className='text-sky-600'>Recrutando agora</a>
                 </div>
                 
                 <article className='p-5'>
@@ -44,18 +44,62 @@ const Report = () => {
                     busca e tenha um histórico de aprendizado contínuo;
                 
                 Responsabilidades e atribuições:
+                Culturalmente, procuramos uma pessoa que:
+                    seja curiosa por natureza;
+                    perfil mão na massa e ágil;
+                    sabe ouvir e se comunicar;
+                    busca e tenha um histórico de aprendizado contínuo;
+                
+                Responsabilidades e atribuições:
+                Culturalmente, procuramos uma pessoa que:
+                    seja curiosa por natureza;
+                    perfil mão na massa e ágil;
+                    sabe ouvir e se comunicar;
+                    busca e tenha um histórico de aprendizado contínuo;
+                
+                Responsabilidades e atribuições:
+                Culturalmente, procuramos uma pessoa que:
+                    seja curiosa por natureza;
+                    perfil mão na massa e ágil;
+                    sabe ouvir e se comunicar;
+                    busca e tenha um histórico de aprendizado contínuo;
+                
+                Responsabilidades e atribuições:
+                Culturalmente, procuramos uma pessoa que:
+                    seja curiosa por natureza;
+                    perfil mão na massa e ágil;
+                    sabe ouvir e se comunicar;
+                    busca e tenha um histórico de aprendizado contínuo;
+                
+                Responsabilidades e atribuições:
+                Culturalmente, procuramos uma pessoa que:
+                    seja curiosa por natureza;
+                    perfil mão na massa e ágil;
+                    sabe ouvir e se comunicar;
+                    busca e tenha um histórico de aprendizado contínuo;
+                
+                Responsabilidades e atribuições:
+                Culturalmente, procuramos uma pessoa que:
+                    seja curiosa por natureza;
+                    perfil mão na massa e ágil;
+                    sabe ouvir e se comunicar;
+                    busca e tenha um histórico de aprendizado contínuo;
+                
+                Responsabilidades e atribuições:
+                Culturalmente, procuramos uma pessoa que:
+                    seja curiosa por natureza;
+                    perfil mão na massa e ágil;
+                    sabe ouvir e se comunicar;
+                    busca e tenha um histórico de aprendizado contínuo;
+                
+                Responsabilidades e atribuições:
+
                 </article>
                 </section> 
                 </div>
                 </div>
-                    <div className='report_write'>
-                    Por que está denunciando esta vaga?<br></br>
-                    Informe abaixo a informação mais relevante para a denuncia da vaga!
-                <hr></hr>
-                <br></br>
-
-                <input type="text" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleText0" placeholder="Descreva a causa da denúncia?"/>
-                </div>
+                
+                   
                 </div>
                 
             </Container>
