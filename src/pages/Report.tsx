f910ebf4bc4dfcbba0af338af235e26ea70c8b7c
Alberto Santos <89992304+allbertuu@users.noreply.github.com>
2022-09-29 15:47:21 -0300

    remove unnecessary import from Report.tsx

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Header from "../components/HeaderReport";
 import { Outlet } from "react-router-dom";
 import { Container } from "../components/styles/Container.styled";
 
