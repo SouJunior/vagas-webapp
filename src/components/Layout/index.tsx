f36e62afb1cfc59eee72ccc119c8399d46a5077b
Mr. Nullus <gustavo.silva1667@etec.sp.gov.br>
2022-10-07 18:20:01 -0300

    feat: refatoring path from styles and components

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 
-import { Container } from "../styles/Container";
+import { Container } from "../../styles/Container";
 import { ExtendedFooter } from "../ExtendedFooter";
 
 export default function Layout() {
