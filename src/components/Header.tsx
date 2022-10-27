a378a3ac55d9875dd9040474642c325789562d6a
CarlosJunioor <carlosjunior866@gmail.com>
2022-10-28 00:51:38 +0100

    bugfix: fixed bug on header and report page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import { RiArrowLeftLine as ArrowLeftIcon } from 'react-icons/ri';
 import { Link, To } from 'react-router-dom';
 import logoIconName from '../assets/imgs/logo-icon-name-h.svg';
 
+
 interface HeaderProps extends HTMLAttributes<HTMLElement> {
     backTo: To;
     pageName: string;
