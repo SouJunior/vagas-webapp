2de256c0afbfdd0e5b092275befd77309045cb0f
silvia_teixeira <quimicasilvia20@gmail.com>
2022-10-31 15:44:28 -0300

    Create component select

---
+++ src/components/Select.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const SelectWrapper = styled.select`
+    
+   height: 50px;
+   width: 535px;
+   border: 1px solid #E8E8E8;
+   border-radius: 6px;
+`;
+
+export default function Select(){
+   return(
+   <SelectWrapper>
+            <option disabled>- Selecione aqui -</option>
+            <option value="Estágio">Estágio</option>
+            <option value="Júnior">Júnior</option>
+            <option value="Trainee">Trainee</option>
+   </SelectWrapper>
+   );
+}
