ece4871a3c35f8b4f5f5584327da0f3cd1ec943d
Alberto Santos <89992304+allbertuu@users.noreply.github.com>
2022-11-05 23:32:57 -0300

    styles: add indent size to 4

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -2,16 +2,16 @@ import { FunctionComponent } from "react";
 import styled from "styled-components";
 
 interface SelectWrapperProps {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 export const SelectWrapper = styled.select`
-  height: 50px;
-  width: 535px;
-  border: 1px solid #e8e8e8;
-  border-radius: 6px;
+    height: 50px;
+    width: 535px;
+    border: 1px solid #e8e8e8;
+    border-radius: 6px;
 `;
 
 export const Select: FunctionComponent<SelectWrapperProps> = ({ children }) => {
-  return <SelectWrapper>{children}</SelectWrapper>;
+    return <SelectWrapper>{children}</SelectWrapper>;
 };
