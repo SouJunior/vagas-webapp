b73d193b0748b9f3a5ed5a399d86307090ac4a43
silvia_teixeira <quimicasilvia20@gmail.com>
2022-11-05 22:55:44 -0300

    Co-authored-by: Alberto Santos <allbertuu@users.noreply.github.com>

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,20 +1,17 @@
+import { FunctionComponent } from "react";
 import styled from "styled-components";
 
+interface SelectWrapperProps {
+  children: React.ReactNode;
+}
+
 export const SelectWrapper = styled.select`
-    
-   height: 50px;
-   width: 535px;
-   border: 1px solid #E8E8E8;
-   border-radius: 6px;
+  height: 50px;
+  width: 535px;
+  border: 1px solid #e8e8e8;
+  border-radius: 6px;
 `;
 
-export default function Select(){
-   return(
-   <SelectWrapper>
-            <option disabled>- Selecione aqui -</option>
-            <option value="Estágio">Estágio</option>
-            <option value="Júnior">Júnior</option>
-            <option value="Trainee">Trainee</option>
-   </SelectWrapper>
-   );
-}
+export const Select: FunctionComponent<SelectWrapperProps> = ({ children }) => {
+  return <SelectWrapper>{children}</SelectWrapper>;
+};
