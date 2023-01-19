3d8197d62c50b03657fbd2599a02b3cbc13e47a8
Igor Gonçalves <igordossantos38@gmail.com>
2023-01-19 10:51:18 -0300

    feat: import svg and insert as a background layer

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,8 +2,12 @@ import { Button } from '../components/Button';
 import extendedLogoImg from '../assets/imgs/logotipo-icone-extendida.svg';
 import LoginCard from '../components/LoginCard';
 
+import CircleDetails from '../assets/imgs/Circles-details.svg'
+
 const Login = () => {
     return (
+        <>
+        <div className='w-full h-screen absolute bg-cover bg-no-repeat' style={{ background: `url(${CircleDetails})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}></div>
         <main className="grid lg:grid-cols-2 px-4 bg-blue-gradient h-screen">
             <section className="lg:pl-16 pl-24 mt-44">
                 <img
@@ -20,7 +24,9 @@ const Login = () => {
                     border="white"
                     className="w-[283px] h-[67px] text-2xl"
                 >
-                    <a href="https://www.soujunior.tech" target="blank">Saiba mais</a>
+                    <a href="https://www.soujunior.tech" target="blank">
+                        Saiba mais
+                    </a>
                 </Button>
             </section>
 
@@ -30,6 +36,7 @@ const Login = () => {
                 </div>
             </section>
         </main>
+        </>
     );
 };
 
