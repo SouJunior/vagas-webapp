8a1053d96f6800e8c24e7a13b043b87f6325adea
Igor Gonçalves <igordossantos38@gmail.com>
2023-01-19 11:34:18 -0300

    chore: format document

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,39 +3,38 @@ import { CirclesDetailsBackground } from './styles/Login.styled';
 import extendedLogoImg from '../assets/imgs/logotipo-icone-extendida.svg';
 import LoginCard from '../components/LoginCard';
 
-
 const Login = () => {
     return (
         <>
-        <CirclesDetailsBackground />
-        <main className="grid lg:grid-cols-2 px-4 bg-blue-gradient h-screen">
-            <section className="lg:pl-16 pl-24 mt-44">
-                <img
-                    className="w-[450px]"
-                    src={extendedLogoImg}
-                    alt="Logo-sou-junior-square"
-                />
-                <h1 className="text-white text-[78px] leading-[94px] my-8">
-                    Onde o Júnior
-                    <br /> tem vez!
-                </h1>
-                <Button
-                    background="outline"
-                    border="white"
-                    className="w-[283px] h-[67px] text-2xl"
-                >
-                    <a href="https://www.soujunior.tech" target="blank">
-                        Saiba mais
-                    </a>
-                </Button>
-            </section>
+            <CirclesDetailsBackground />
+            <main className="grid lg:grid-cols-2 px-4 bg-blue-gradient h-screen">
+                <section className="lg:pl-16 pl-24 mt-44">
+                    <img
+                        className="w-[450px]"
+                        src={extendedLogoImg}
+                        alt="Logo-sou-junior-square"
+                    />
+                    <h1 className="text-white text-[78px] leading-[94px] my-8">
+                        Onde o Júnior
+                        <br /> tem vez!
+                    </h1>
+                    <Button
+                        background="outline"
+                        border="white"
+                        className="w-[283px] h-[67px] text-2xl"
+                    >
+                        <a href="https://www.soujunior.tech" target="blank">
+                            Saiba mais
+                        </a>
+                    </Button>
+                </section>
 
-            <section>
-                <div>
-                    <LoginCard />
-                </div>
-            </section>
-        </main>
+                <section>
+                    <div>
+                        <LoginCard />
+                    </div>
+                </section>
+            </main>
         </>
     );
 };
