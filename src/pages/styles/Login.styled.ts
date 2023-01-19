0136b7027c89e7877565ee9d371712257f7b6697
Igor Gonçalves <igordossantos38@gmail.com>
2023-01-19 11:23:55 -0300

    style: create a styled-component and apply atributes

--- src/pages/styles/Login.styled.ts
+++ src/pages/styles/Login.styled.ts
@@ -1,1 +1,12 @@
 import styled from 'styled-components';
+
+import circleDetaisl from '../../assets/imgs/Circles-details.svg'
+
+export const CirclesDetailsBackground = styled.div`
+    width: 100%;
+    height: 100vh;
+    position: absolute;
+    background-image: url(${circleDetaisl});
+    background-size: cover;
+    background-repeat: no-repeat;
+`
