4e6da405145a5545d64c3fde3845b0fb4ca92925
Igor Gonçalves <igordossantos38@gmail.com>
2023-02-01 12:03:52 -0300

    refactor: change semantic structure and position elements

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,39 +2,44 @@ import { Button } from '../components/Button';
 import extendedLogoImg from '../assets/imgs/logotipo-icone-extendida.svg';
 import LoginCard from '../components/LoginCard';
 
-import '../App.css'
+import '../App.css';
 
 const Login = () => {
     return (
-        <main className="grid lg:grid-cols-2 px-4 bg-hero-pattern bg-cover bg-no-repeat z-50 h-screen">
-                <section className="lg:pl-16 pl-24 mt-44">
-                    <img
-                        className="w-[450px]"
-                        src={extendedLogoImg}
-                          alt="Logo-sou-junior-square"
-                    />
-                    <h1 className="text-white text-[78px] leading-[94px] my-8">
-                        Onde o Júnior
-                        <br /> tem vez!
-                    </h1>
-                    <Button
-                        background="outline"
-                        border="white"
-                        className="w-[283px] h-[67px] text-2xl"
-                        style={{ position: 'absolute' }}
-                        onClick={()=> window.open('https://www.soujunior.tech', '_blank')}
-                    >
+        <main className="bg-hero-pattern bg-cover bg-no-repeat h-screen">
+            <div className="container max-w-screen-lg m-auto">
+                <section className="flex justify-center items-center">
+                    <div className="titles">
+                        <img
+                            className="w-[450px]"
+                            src={extendedLogoImg}
+                            alt="Logo-sou-junior-square"
+                        />
+                        <h1 className="text-white text-[78px] leading-[94px] my-8">
+                            Onde o Júnior
+                            <br /> tem vez!
+                        </h1>
+                        <Button
+                            background="outline"
+                            border="white"
+                            className="w-[283px] h-[67px] text-2xl"
+                            style={{ position: 'absolute' }}
+                            onClick={() =>
+                                window.open(
+                                    'https://www.soujunior.tech',
+                                    '_blank',
+                                )
+                            }
+                        >
                             Saiba mais
-    
-                    </Button>
-                </section>
-
-                <section>
-                    <div>
+                        </Button>
+                    </div>
+                    <div className='login-card'>
                         <LoginCard />
                     </div>
                 </section>
-            </main>
+            </div>
+        </main>
     );
 };
 
