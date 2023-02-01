6d52c3acf95b74a940f1b6540735f40e744b5abd
RafaelPires2 <rafaelbrpires@gmail.com>
2023-02-01 10:41:28 -0300

    feature: register page with styles of login page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,36 +2,39 @@ import { Button } from '../components/Button';
 import extendedLogoImg from '../assets/imgs/logotipo-icone-extendida.svg';
 import { RegisterCard } from '../components/RegisterCard';
 
-// Refatorar tela com component criado pelo Igor
+import '../App.css'
+
 const Register = () => {
     return (
-        <main className="grid lg:grid-cols-2 px-4 bg-blue-gradient h-screen">
-            <section className="lg:pl-16 pl-24 mt-44">
-                <img
-                    className="w-[450px]"
-                    src={extendedLogoImg}
-                    alt="Logo-sou-junior-square"
-                />
-                <h1 className="text-white text-[78px] leading-[94px] my-8">
-                    Onde o Júnior
-                    <br /> tem vez!
-                </h1>
-                <Button
-                    background="outline"
-                    border="white"
-                    className="w-[283px] h-[67px] text-2xl"
-                    onClick={() => window.open('www.soujunior.tech', '_black')}
-                >
-                    Saiba mais
-                </Button>
-            </section>
+        <main className="grid lg:grid-cols-2 px-4 bg-hero-pattern bg-cover bg-no-repeat z-50 h-screen d-flex">
+                <section className="lg:pl-16 pl-24 mt-44">
+                    <img
+                        className="w-[450px]"
+                        src={extendedLogoImg}
+                          alt="Logo-sou-junior-square"
+                    />
+                    <h1 className="text-white text-[78px] leading-[94px] my-8">
+                        Onde o Júnior
+                        <br /> tem vez!
+                    </h1>
+                    <Button
+                        background="outline"
+                        border="white"
+                        className="w-[283px] h-[67px] text-2xl"
+                        style={{ position: 'absolute' }}
+                        onClick={()=> window.open('https://www.soujunior.tech', '_blank')}
+                    >
+                            Saiba mais
+    
+                    </Button>
+                </section>
 
-            <section>
-                <div>
-                    <RegisterCard />
-                </div>
-            </section>
-        </main>
+                <section className="flex justify-center items-center">
+                    <div>
+                        <RegisterCard />
+                    </div>
+                </section>
+            </main>
     );
 };
 
