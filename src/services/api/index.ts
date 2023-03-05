65f461ab7888f6d02e42e48ff7aa684392df21cb
Igor Gonçalves <igordossantos38@gmail.com>
2023-03-05 20:23:30 -0300

    feat: create a mocked object to simulate a request

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_URL
+  // baseURL: process.env.REACT_APP_URL
+  baseURL: '../../mock/user.json'
 });
 
 export default api;
