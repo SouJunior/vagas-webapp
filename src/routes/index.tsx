5437ec526898e6a3344d4127a3d9eafb3a7c7172
Igor Gonçalves <igordossantos38@gmail.com>
2023-03-05 19:22:37 -0300

    feat: create a route file to reference allRoutes

---
+++ src/routes/index.tsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import Layout from '../components/Layout';
+import Report from '../pages/Report';
+import ErrorPage from '../pages/ErrorPage';
+
+import { InsertJobs } from '../pages/InsertJobs';
+
+export const AllRoutes: React.FC = () => {
+    return (
+        <Routes>
+        <Route index element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="/" element={<Layout />}>
+            <Route path="insertjobs" element={<InsertJobs />} />
+            <Route path="report/:id" element={<Report />} />
+            <Route path="*" element={<ErrorPage />} />
+        </Route>
+    </Routes>
+    )
+}
