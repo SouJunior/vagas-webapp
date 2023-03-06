f3bf6a3214b1831b1257d252ef52b5f7d350c3ff
Igor Gonçalves <igordossantos38@gmail.com>
2023-03-05 23:49:21 -0300

    feat: require login to render page InsertJobs

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,17 +7,25 @@ import Report from '../pages/Report';
 import ErrorPage from '../pages/ErrorPage';
 
 import { InsertJobs } from '../pages/InsertJobs';
+import { RequireAuth } from '../contexts/Auth/RequireAuth';
 
 export const AllRoutes: React.FC = () => {
     return (
         <Routes>
-        <Route index element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route path="/" element={<Layout />}>
-            <Route path="insertjobs" element={<InsertJobs />} />
-            <Route path="report/:id" element={<Report />} />
-            <Route path="*" element={<ErrorPage />} />
-        </Route>
-    </Routes>
-    )
-}
+            <Route index element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="/" element={<Layout />}>
+                <Route
+                    path="insertjobs"
+                    element={
+                        <RequireAuth>
+                            <InsertJobs />
+                        </RequireAuth>
+                    }
+                />
+                <Route path="report/:id" element={<Report />} />
+                <Route path="*" element={<ErrorPage />} />
+            </Route>
+        </Routes>
+    );
+};
