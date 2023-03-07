b08e3ce082eab20242289bc4e8b44744e81679ea
Igor Gonçalves <igordossantos38@gmail.com>
2023-03-07 08:52:07 -0300

    feat: remove fake returning of datas

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,24 +1,15 @@
-import api from "../services/api";
+import api from '../services/api';
 
 export const useApi = () => ({
+    //TODO: Entrar com as funções que salvam o token de usuário
 
     signin: async (email: string, password: string) => {
-        // Retornando dados falsos
-        return {
-            user: {
-                "id": 1,
-                "email": "rafa@gmail.com",
-                "password": "Rafa12@#",
-                "token": "ashfadfd"
-            }
-        }
-        // const res = await api.post("/signin", { email, password })
-        // return res.data
+        const res = await api.post('/signin', { email, password });
+        return res.data;
     },
 
     signout: async () => {
-        const res = await api.post("/logout");
+        const res = await api.post('/signout');
         return res.data;
-    }
-
+    },
 });
