cdd3d17113b60d38b3099199d94171e693fa8af4
Igor Gonçalves <igordossantos38@gmail.com>
2023-03-09 10:13:38 -0300

    refact: format code

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_URL
+    baseURL: process.env.REACT_APP_URL,
 });
 
 export default api;
