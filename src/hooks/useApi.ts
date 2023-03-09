1b09913caa8aac8aebb27b44313c242ffafd8148
Igor Gonçalves <igordossantos38@gmail.com>
2023-03-09 10:11:26 -0300

    refact: change name of funtions and formating code

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -3,13 +3,16 @@ import api from '../services/api';
 export const useApi = () => ({
     //TODO: Entrar com as funções que salvam o token de usuário
 
-    signin: async (email: string, password: string) => {
-        const res = await api.post('/signin', { email, password });
+    login: async (email: string, password: string) => {
+        const res: any = await api.post('/auth/login', {
+            email,
+            password,
+        });
         return res.data;
     },
 
-    signout: async () => {
-        const res = await api.post('/signout');
+    logout: async () => {
+        const res = await api.post('/logout');
         return res.data;
     },
 });
