a257a2528e1180347ed066d78cbb09082804f5d7
Igor Gonçalves <igordossantos38@gmail.com>
2023-03-12 20:43:21 -0300

    feat: storage token in localStorage

--- src/contexts/Auth/AuthProvider.tsx
+++ src/contexts/Auth/AuthProvider.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { useApi } from '../../hooks/useApi';
 import { User } from '../../@types/User';
@@ -9,17 +9,36 @@ export const AuthProvider = ({ children }: { children: JSX.Element }) => {
 
     const api = useApi();
 
+    // Vai executar uma vez sempre que o componente for renderizado
+    useEffect(() => {
+        const validateToken = async () => {
+            const storagedData = localStorage.getItem('authToken')
+            if(storagedData) {
+                const data = await api.validateToken(storagedData)
+                if(data.user) {
+                    setUser(data.user);
+                }
+            }
+        }
+        validateToken();
+    }, [api])
+
     const login = async (email: string, password: string) => {
         const data: any = await api.login(email, password);
 
-        if (data.user) {
+        if (data.user && data.token) {
             setUser(data.user);
+            setToken(data.token)
             return true;
         }
         return false;
     };
 
     const logout = async () => {};
+
+    const setToken = (token: string) => {
+        localStorage.setItem('authToken', token)
+    }
 
     return (
         <AuthContext.Provider value={{ user, login, logout }}>
