baa8c1ddce93fde887f9263579fb393c07b2cdd2
Igor Gonçalves <igordossantos38@gmail.com>
2023-03-15 22:47:29 -0300

    refact: delete unused page Register
    All process of register is made in Login card component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Login from '../pages/Login';
-import Register from '../pages/Register';
 import Layout from '../components/Layout';
 import Report from '../pages/Report';
 import ErrorPage from '../pages/ErrorPage';
 
 import { InsertJobs } from '../pages/InsertJobs';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { RequireAuth } from '../contexts/Auth/RequireAuth';
 
 export const AllRoutes: React.FC = () => {
     return (
         <Routes>
             <Route index element={<Login />} />
-            <Route path="register" element={<Register />} />
             <Route path="/" element={<Layout />}>
                 <Route path="insertjobs" element={<InsertJobs />} />
                 <Route path="report/:id" element={<Report />} />
