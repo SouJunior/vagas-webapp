c6bd9f92560fc1d3caeae53877a152e700205abf
Igor Gonçalves <igordossantos38@gmail.com>
2023-03-15 23:33:16 -0300

    feat: insert routes of pages created

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,15 +5,24 @@ import Layout from '../components/Layout';
 import Report from '../pages/Report';
 import ErrorPage from '../pages/ErrorPage';
 
+import { Home } from '../pages/Home';
 import { InsertJobs } from '../pages/InsertJobs';
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { RequireAuth } from '../contexts/Auth/RequireAuth';
+import { FeedJobs } from '../pages/FeedJobs';
 
 export const NavRoutes: React.FC = () => {
     return (
         <Routes>
-            <Route index element={<Login />} />
+            <Route index element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route
+                path="/feedjobs"
+                element={
+                    <RequireAuth>
+                        <FeedJobs />
+                    </RequireAuth>
+                }
+            />
             <Route path="/" element={<Layout />}>
                 <Route path="insertjobs" element={<InsertJobs />} />
                 <Route path="report/:id" element={<Report />} />
