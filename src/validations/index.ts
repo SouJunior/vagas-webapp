ad23a812d89e242fc012db16813c2cd81bafe5b6
Igor Gonçalves <igordossantos38@gmail.com>
2023-03-20 23:07:26 -0300

    bugfix: error on validations file
    An error on validation part of register user is preventing
    a submit default by react hook form

--- src/validations/index.ts
+++ src/validations/index.ts
@@ -67,33 +67,36 @@ export const formSchema = yup.object().shape({
       /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W+)(?=^.{6,20}$).*$/,
       "A senha deve conter 1 letra maiúscula, 1 minúscula, 1 número, 1 caracter especial !@#$% etc..."
     ),
-    registerEmail: yup
-    .string()
-    .required("O campo Email é obrigatório")
-    .email("Email precisa ser válido. Exemplo: email@gmail.com"),
-    registerPassword: yup
-    .string()
-    .required("O campo Senha é obrigatório")
-    .min(6, "A senha precisa ter no mínimo 6 caracteres")
-    .max(20, "A senha precisa ter no máximo 20 caracteres")
-    .matches(
-      /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W+)(?=^.{6,20}$).*$/,
-      "A senha deve conter 1 letra maiúscula, 1 minúscula, 1 número, 1 caracter especial !@#$% etc..."
-    ),
-    confirmPassword: yup
-    .string()
-    .required("Confirme sua senha")
-    .oneOf([yup.ref("registerPassword"), null], "As senhas precisam ser iguais"),
-    registerName: yup
-    .string()
-    .required("O campo de nome é obrigatório"),
-    registerCnpj: yup
-    .string()
-    .required("CNPJ é obrigatório")
-    .matches(/^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/, 'CNPJ inválido')
-    .test('test-cnpj', 'CNPJ inválido', (cnpj) => validarCNPJ(cnpj ?? '')),
-    privacyTerms: yup
-    .bool()
-    .required("Você deve aceitar os termos de privacidade para continuar")
-    .oneOf([true], "Você deve aceitar os termos de privacidade para continuar")
+    //~~! Essa parte da validação do formulário está causando
+    //~~! um erro no hookForm.
+    
+    // registerEmail: yup
+    // .string()
+    // .required("O campo Email é obrigatório")
+    // .email("Email precisa ser válido. Exemplo: email@gmail.com"),
+    // registerPassword: yup
+    // .string()
+    // .required("O campo Senha é obrigatório")
+    // .min(6, "A senha precisa ter no mínimo 6 caracteres")
+    // .max(20, "A senha precisa ter no máximo 20 caracteres")
+    // .matches(
+    //   /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W+)(?=^.{6,20}$).*$/,
+    //   "A senha deve conter 1 letra maiúscula, 1 minúscula, 1 número, 1 caracter especial !@#$% etc..."
+    // ),
+    // confirmPassword: yup
+    // .string()
+    // .required("Confirme sua senha")
+    // .oneOf([yup.ref("registerPassword"), null], "As senhas precisam ser iguais"),
+    // registerName: yup
+    // .string()
+    // .required("O campo de nome é obrigatório"),
+    // registerCnpj: yup
+    // .string()
+    // .required("CNPJ é obrigatório")
+    // .matches(/^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/, 'CNPJ inválido')
+    // .test('test-cnpj', 'CNPJ inválido', (cnpj) => validarCNPJ(cnpj ?? '')),
+    // privacyTerms: yup
+    // .bool()
+    // .required("Você deve aceitar os termos de privacidade para continuar")
+    // .oneOf([true], "Você deve aceitar os termos de privacidade para continuar")
 });
