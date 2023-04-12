9b6b2fac790bad64fe0ed166bcdda4ae3bdb7215
Igor Gonçalves <igordossantos38@gmail.com>
2023-04-11 23:21:31 -0300

    style: finish style in elements #134

--- src/components/LoginCard/PopUpRegisterSuccess/styles.ts
+++ src/components/LoginCard/PopUpRegisterSuccess/styles.ts
@@ -1,33 +1,45 @@
-import styled from "styled-components"
+import styled from 'styled-components';
 
 export const Popup = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
+    align-items: center;
 
     width: 100%;
     max-width: 620px;
-    height: 400px;
-    
+
     position: absolute;
 
-    border: 2px solid #1165BA;
-    border-radius: 10px;
-    background: #FDFFFC;
-    
-    img {
+    top: 25%;
+    left: 25%;
 
+    border: 2px solid #1165ba;
+    border-radius: 10px;
+    background: #fdfffc;
+
+    .email {
+        width: 22%;
+        margin: 13px;
     }
 
     img.close {
-
+        width: 6%;
+        margin: 16px 24px 0 auto;
     }
 
     h2 {
-
+        color: #1165ba;
+        font-size: 1.9em;
+        font-weight: 600;
+        margin: 45px 0 16px 0;
     }
 
     p {
-
+        text-align: center;
+        color: #5d5f5d;
+        font-size: inherit;
+        font-weight: bold;
+        margin: 0 90px 70px 90px;
     }
-`
+`;
