a856782d17e56d26544f147fe7604d4d0b2fd508
Igor Gonçalves <igordossantos38@gmail.com>
2023-04-11 22:10:30 -0300

    style: apply previews style in component #134

---
+++ src/components/LoginCard/PopUpRegisterSuccess/styles.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components"
+
+export const Popup = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    width: 100%;
+    max-width: 620px;
+    height: 400px;
+    
+    position: absolute;
+
+    border: 2px solid #1165BA;
+    border-radius: 10px;
+    background: #FDFFFC;
+    
+    img {
+
+    }
+
+    img.close {
+
+    }
+
+    h2 {
+
+    }
+
+    p {
+
+    }
+`
