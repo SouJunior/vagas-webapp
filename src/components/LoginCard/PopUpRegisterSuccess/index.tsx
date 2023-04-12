d562d8cfb798e018b7ff5e5755e5c69b139bb791
Igor Gonçalves <igordossantos38@gmail.com>
2023-04-12 00:15:28 -0300

    refact: add button to handle event onClick #134

--- src/components/LoginCard/PopUpRegisterSuccess/index.tsx
+++ src/components/LoginCard/PopUpRegisterSuccess/index.tsx
@@ -6,13 +6,14 @@ import { Popup } from './styles';
 export const PopUpRegisterSucess = (props: any): JSX.Element => {
     return (
         <Popup>
-            <img className='close' src={close} alt="botão fechar" />
-            <img className='email' src={popup} alt="Email checked" />
+            <button onClick={props.close}>
+                <img className="close" src={close} alt="botão fechar" />
+            </button>
+            <img className="email" src={popup} alt="Email checked" />
             <h2>Cadastro concluído com sucesso!</h2>
             <p>
-                Enviamos um link de validação para o e-mail
-                "{props.email}". Verifique sua caixa de entrada
-                para fazer o login.
+                Enviamos um link de validação para o e-mail "{props.email}".
+                Verifique sua caixa de entrada para fazer o login.
             </p>
         </Popup>
     );
