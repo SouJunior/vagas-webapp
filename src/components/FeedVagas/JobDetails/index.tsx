f7c0e627529c78a4849ef833f33b4d21d72d167c
João Vitor Melo Kremer <joaokremer.dev@gmail.com>
2023-04-14 19:44:18 -0300

    style:(candidato) add styles at the jobDetails (#139)

--- src/components/FeedVagas/JobDetails/index.tsx
+++ src/components/FeedVagas/JobDetails/index.tsx
@@ -4,7 +4,11 @@ import {
     Company,
     Container,
     Description,
+    DescriptionInfo,
+    ExtraInfoContainer,
+    ExtraInfoItem,
     Location,
+    PaddingContainer,
     Prerequisites,
     Title,
     Type,
@@ -12,10 +16,16 @@ import {
 import { useApi } from '../../../hooks/useApi';
 import { Job } from '../../../@types/jobs';
 import JobDetailsSkeleton from './jobDetailsSkeleton';
+import { Link } from 'react-router-dom';
+import { object } from 'yup';
 
 function JobDetails({ id, clickedJob }: { id: any; clickedJob: any }) {
     const [jobDetails, setJobDetails] = useState<Job | null>(null);
     const api = useApi();
+
+    if (Array.isArray(clickedJob)) {
+        clickedJob = Object.assign({}, ...clickedJob);
+    }
 
     // useEffect(() => {
     //     let timeoutId: NodeJS.Timeout;
@@ -40,35 +50,56 @@ function JobDetails({ id, clickedJob }: { id: any; clickedJob: any }) {
     //     };
     // }, [id]);
 
-
     // Tirei a requisição para testar uma possível melhoria na aquisição desses dados. Pois a requisição
     // estava redundante e sendo feita "sem necessidade".
 
     return (
         <Container>
-            
-                <div>
-                    {clickedJob[0]?.title && (
-                        <Title>{clickedJob[0].title}</Title>
+            <div>
+                <PaddingContainer>
+                    {clickedJob?.title && <Title>{clickedJob.title}</Title>}
+                    {clickedJob?.title && <Company>Empresa Verde</Company>}
+                    {clickedJob?.title && (
+                        <Location>
+                            Bento Gonçalves, Rio Grande do Sul, Brasil{' '}
+                        </Location>
                     )}
-                    {clickedJob[0]?.description && (
-                        <Description>{clickedJob[0].description}</Description>
+
+                    {clickedJob?.headquarters && (
+                        <Company>{clickedJob.headquarters}</Company>
                     )}
-                    {clickedJob[0]?.headquarters && (
-                        <Company>{clickedJob[0].headquarters}</Company>
-                    )}
-                    {clickedJob[0]?.modality &&
-                        clickedJob[0]?.type &&
-                        clickedJob[0]?.typeContract && (
+                    {clickedJob?.modality &&
+                        clickedJob?.type &&
+                        clickedJob?.typeContract && (
                             <Type>
-                                {clickedJob[0].modality} - {clickedJob[0].type}{' '}
-                                - {clickedJob[0].typeContract}
+                                {clickedJob.modality} - {clickedJob.type} -{' '}
+                                {clickedJob.typeContract}
                             </Type>
                         )}
-                    {clickedJob[0]?.contractType && (
-                        <Description>{clickedJob[0].contractType}</Description>
-                    )}
-                </div>
+                </PaddingContainer>
+                {clickedJob?.contractType && (
+                    <ExtraInfoContainer>
+                        <ExtraInfoItem colorActive={false}>
+                            {clickedJob.contractType}
+                        </ExtraInfoItem>
+                        <ExtraInfoItem colorActive={false}>
+                            {'51-200 funcionários'}
+                        </ExtraInfoItem>
+                        <ExtraInfoItem colorActive={true}>
+                            <Link to="">{'Recrutando agora'}</Link>
+                        </ExtraInfoItem>
+                    </ExtraInfoContainer>
+                )}
+                <PaddingContainer>
+                    {
+                        <DescriptionInfo>
+                            {
+                                'Procuramos um profissional para ocupar a posição de UX Designer em um cliente referência no setor de produção de imunobiológicos (vacinas) e análises laboratoriais veterinários.'
+                            }
+                        </DescriptionInfo>
+                    }
+                </PaddingContainer>
+            </div>
         </Container>
     );
 }
