ec20297bff73b431ec548abbb715885412d618c7
Igor Gonçalves <igordossantos38@gmail.com>
2023-04-18 21:06:16 -0300

    style: change style to button

--- src/components/styles/Button.styled.ts
+++ src/components/styles/Button.styled.ts
@@ -11,11 +11,12 @@ interface ButtonWrapperProps {
 }
 
 export const ButtonWrapper = styled.button<ButtonWrapperProps | any>`
-    padding: 8px 12px;
+    padding: 8px 48px;
     background-color: ${({ theme }) => theme.colors.primary};
     border: 1px solid #004892;
     border-radius: 4px;
-    color: #fff;
+    font-weight: bolder;
+    font-size: 1.4em;
 
     min-width: fit-content;
     height: ${(props) => props.height};
@@ -34,15 +35,15 @@ export const ButtonWrapper = styled.button<ButtonWrapperProps | any>`
     ${({ background }) =>
         background === 'outline' &&
         css`
-            background: transparent;
+            background: ${({ theme }) => theme.colors.secondaryLight};
             border-color: ${({ theme }) => theme.colors.primary};
-            color: ${({ theme }) => theme.colors.primary};
+            color: ${({ theme }) => theme.colors.primaryDark};
         `}
     ${({ border }) =>
         border === 'white' &&
         css`
             border-width: 2px;
-            border-color: ${({ theme }) => theme.colors.muted};
-            color: ${({ theme }) => theme.colors.muted};
+            border-color: ${({ theme }) => theme.colors.secondaryLight};
+            color: ${({ theme }) => theme.colors.primaryDark};
         `}
 `;
