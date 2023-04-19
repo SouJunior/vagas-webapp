9945afd3653d72afd3655f381acb86d17f9fa0c4
Igor Gonçalves <igordossantos38@gmail.com>
2023-04-18 21:34:59 -0300

    feat: update in login page design and layout #130

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,30 +1,35 @@
 import { Button } from '../components/Button';
 import extendedLogoImg from '../assets/imgs/logotipo-icone-extendida.svg';
+import loginPeople from '../assets/imgs/login-people.svg';
+import verticalLine from '../assets/imgs/verticalLine.svg';
 import LoginCard from '../components/LoginCard';
 
 import '../App.css';
+import { AchorLink } from './styles/Main.styled';
 
 const Login = () => {
     return (
         <main className="bg-hero-pattern bg-cover bg-no-repeat w-full h-screen md:w-full overflow-y-auto">
             <div className="container max-w-screen-xl m-auto h-screen">
-                <section className="flex flex-col justify-between items-center h-screen lg:flex-row lg:mb-20">
-                    <div className="titles flex flex-col items-center mb-0 mx-auto mt-10 lg:items-start lg:mt-0 lg:mb-36">
+                <section className="flex flex-col items-center h-screen lg:flex-row lg:mb-20">
+                    <div className="titles max-h-[725px] flex flex-col flex-grow items-center lg:items-center h-5/6">
                         <img
-                            className="max-w-md"
+                            className="max-w-md flex-grow"
                             src={extendedLogoImg}
                             alt="Logo-sou-junior-square"
                         />
-                        <div className="max-w-xl text-center lg:text-start">
-                            <h1 className="text-white text-7xl font-canada leading-tight my-9">
-                                O <b>Júnior</b> de hoje é o <b>Sênior</b> de
-                                amanhã!
-                            </h1>
+                        <div className="max-w-xl flex-grow text-center lg:text-center">
+                            <img src={loginPeople} alt="" />
                         </div>
                         <Button
                             background="outline"
                             border="white"
-                            className="w-[283px] h-[67px] text-2xl"
+                            className="w-[283px] flex-shrink h-[67px] text-2xl"
+                            // inserir rota de navegação para voltar a página home
+                        >
+                            Acessar portal de vagas
+                        </Button>
+                        <AchorLink
                             onClick={() =>
                                 window.open(
                                     'https://www.soujunior.tech',
@@ -32,10 +37,13 @@ const Login = () => {
                                 )
                             }
                         >
-                            Saiba mais
-                        </Button>
+                            Conheça SouJunior Tech
+                        </AchorLink>
                     </div>
-                    <div className="login-card flex mx-auto">
+                    <div className="flex-grow-[.5]">
+                        <img src={verticalLine} alt="" />
+                    </div>
+                    <div className="login-card flex">
                         <LoginCard />
                     </div>
                 </section>
