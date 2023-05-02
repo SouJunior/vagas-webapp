df0531b2ef844db20f2796927ee6e7ee47b8d74a
Igor Gonçalves <igordossantos38@gmail.com>
2023-05-02 18:16:32 -0300

    feat: validity token and save on localstorage

--- src/contexts/Auth/AuthProvider.tsx
+++ src/contexts/Auth/AuthProvider.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useApi } from '../../hooks/useApi';
 import { User } from '../../@types/User';
 import { AuthContext } from './AuthContext';
@@ -9,25 +9,36 @@ export const AuthProvider = ({ children }: { children: JSX.Element }) => {
     const api = useApi();
 
     // Vai executar uma vez sempre que o componente for renderizado
-    // useEffect(() => {
+    useEffect(() => {
+        validateToken();
+    }, []);
 
-    // }, [api]);
-    
-    // const validateToken = async () => {
-    //     const storagedData = localStorage.getItem('authToken');
-        
-    //     if (storagedData) {
-    //         const res = await api.validateToken(storagedData);
-    //         if (res.info) {
-    //             setUser(res.info);
-    //         }
-    //     }
-    // };
-    // validateToken();
+    /**
+     * For this es-lint error; 
+     * @see https://github.com/facebook/react/issues/14920
+     */
+    const validateToken = useCallback(async () => {
+        const storagedToken = localStorage.getItem('authToken');
+
+        if (storagedToken) {
+            // Autoriza retorno dos dados do usuário recebendo token no header
+            // Após realizar login o estado de user é alterado 2 vezes
+            // 1 vez quando faz login
+            // 1 vez quando faz validação do token
+            // Talvez por se tratar do valor user seja melhor o uso de useMemo()
+            const res = await api.validateToken(storagedToken);
+            console.log(res)
+            if (res) {
+                setUser(res);
+            }
+        }
+    }, []);
 
     const login = async (email: string, password: string, type: string) => {
+        // Dados que vem dos inputs da aplicação no momento em que o contexto instanciado
+        // Dados serão passados no corpo da requisição utlizando o método que vem de useApi
         const res = await api.login(email, password, type);
-        
+
         if (res.info && res.token) {
             setUser(res.info);
             setToken(res.token);
@@ -35,18 +46,24 @@ export const AuthProvider = ({ children }: { children: JSX.Element }) => {
         }
         return false;
     };
-    
+
     const setToken = (token: string) => {
+        // Esse método espera 2 params: chave e valor
         localStorage.setItem('authToken', token);
     };
-    
+
     const register = async (
         name: string,
         email: string,
         password: string,
         confirmPassword: string,
     ) => {
-        const res: any = await api.registerUser(name, email, password, confirmPassword);
+        const res: any = await api.registerUser(
+            name,
+            email,
+            password,
+            confirmPassword,
+        );
 
         if (res.data) {
             setUser(res.data.info);
@@ -62,7 +79,13 @@ export const AuthProvider = ({ children }: { children: JSX.Element }) => {
         password: string,
         passwordConfirmation: string,
     ) => {
-        const res: any = await api.registerCompany(companyName, email, cnpj, password, passwordConfirmation);
+        const res: any = await api.registerCompany(
+            companyName,
+            email,
+            cnpj,
+            password,
+            passwordConfirmation,
+        );
 
         if (res.data) {
             setUser(res.data.info);
@@ -74,7 +97,9 @@ export const AuthProvider = ({ children }: { children: JSX.Element }) => {
     const logout = async () => {};
 
     return (
-        <AuthContext.Provider value={{ user, login, register, registerCompany, logout }}>
+        <AuthContext.Provider
+            value={{ user, login, register, registerCompany, logout }}
+        >
             {children}
         </AuthContext.Provider>
     );
