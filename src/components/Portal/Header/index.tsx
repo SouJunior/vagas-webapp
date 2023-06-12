b0097bc3f5979ba9c12ddcee9334e71ef892f898
Igor Gonçalves <igordossantos38@gmail.com>
2023-06-12 17:43:30 -0300

    feat: insert useNavigate and format document

--- src/components/Portal/Header/index.tsx
+++ src/components/Portal/Header/index.tsx
@@ -6,46 +6,62 @@ import {
     Email,
     MenuPicture,
     HeaderLogo,
-    UserInfo
-} from "./styles"
-import Menu from "../Menu"
-import LogoImage from "../../../assets/imgs/logotipo-icone-extendida.svg"
-import MenuImage from "../../../assets/imgs/vertical-menu.svg"
-import ProfileImage from "../../../assets/imgs/profile-image.svg"
+    UserInfo,
+} from './styles';
+import Menu from '../Menu';
+import LogoImage from '../../../assets/imgs/logotipo-icone-extendida.svg';
+import MenuImage from '../../../assets/imgs/vertical-menu.svg';
+import ProfileImage from '../../../assets/imgs/profile-image.svg';
 
-import { AuthContext } from "../../../contexts/Auth/AuthContext"
-import { useContext, useState } from "react"
+import { AuthContext } from '../../../contexts/Auth/AuthContext';
+import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router';
 
 const Index = () => {
-
     const auth: any = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const handleNavigation = () => {
+        navigate('/handle-profile');
+    };
 
     const [visible, setVisible] = useState(false);
 
     return (
         <>
-         <Header>
-            <div>
-                <HeaderLogo src={LogoImage} width="194px" height="29px" alt="logo" />
-            </div>
-            <UserButton>
-                {/* TODO: Vai redirecionar para a pagina de alteração
-                    do perfil de usuário tipo empresa/candidato
-                */}
-                <a href="/">
-                    <ProfilePicture src={ProfileImage} alt="foto de perfil"/>
-                </a>
-                <UserInfo onClick={() => setVisible(!visible)}>
-                    {auth.user.type === "USER" ? (<Name>{auth.user.name}</Name>) : (
-                        <Name>{auth.user.companyName}</Name>)}
-                    <Email>{auth.user.email}</Email>
-                </UserInfo>
-                <MenuPicture onClick={() => setVisible(!visible)} src={MenuImage} alt="menu" />
-            </UserButton>
-        </Header>
-        { visible && <Menu /> }
+            <Header>
+                <div>
+                    <HeaderLogo
+                        src={LogoImage}
+                        width="194px"
+                        height="29px"
+                        alt="logo"
+                    />
+                </div>
+                <UserButton>
+                    <ProfilePicture
+                        onClick={handleNavigation}
+                        src={ProfileImage}
+                        alt="foto de perfil"
+                    />
+                    <UserInfo onClick={() => setVisible(!visible)}>
+                        {auth.user.type === 'USER' ? (
+                            <Name>{auth.user.name}</Name>
+                        ) : (
+                            <Name>{auth.user.companyName}</Name>
+                        )}
+                        <Email>{auth.user.email}</Email>
+                    </UserInfo>
+                    <MenuPicture
+                        onClick={() => setVisible(!visible)}
+                        src={MenuImage}
+                        alt="menu"
+                    />
+                </UserButton>
+            </Header>
+            {visible && <Menu />}
         </>
-    )
-}
+    );
+};
 
-export default Index
+export default Index;
