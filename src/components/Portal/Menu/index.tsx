0bcda659327ad3fe0893bf8829cfaeae7ea64b0a
Igor Gonçalves <igordossantos38@gmail.com>
2023-06-12 17:47:37 -0300

    feat: insert navigation and refactoring removing ancors of li

--- src/components/Portal/Menu/index.tsx
+++ src/components/Portal/Menu/index.tsx
@@ -1,20 +1,26 @@
-import { Container, MenuItems, PopUp, Border } from "./styles"
-import ProfileIcon from "../../../assets/imgs/profile.svg"
-import NewJobIcon from "../../../assets/imgs/new-job.svg"
-import ConfigIcon from "../../../assets/imgs/config.svg"
-import TermsIcon from "../../../assets/imgs/terms.svg"
-import ExitIcon from "../../../assets/imgs/exit.svg"
-import PasswordIcon from "../../../assets/imgs/password.svg"
-import UpArrowImage from "../../../assets/imgs/arrow-up.svg"
-import DownArrowImage from "../../../assets/imgs/arrow-down.svg"
-import { AuthContext } from "../../../contexts/Auth/AuthContext"
-import { useContext, useState } from "react"
+import { Container, MenuItems, PopUp, Border } from './styles';
+import ProfileIcon from '../../../assets/imgs/profile.svg';
+import NewJobIcon from '../../../assets/imgs/new-job.svg';
+import ConfigIcon from '../../../assets/imgs/config.svg';
+import TermsIcon from '../../../assets/imgs/terms.svg';
+import ExitIcon from '../../../assets/imgs/exit.svg';
+import PasswordIcon from '../../../assets/imgs/password.svg';
+import UpArrowImage from '../../../assets/imgs/arrow-up.svg';
+import DownArrowImage from '../../../assets/imgs/arrow-down.svg';
+import { AuthContext } from '../../../contexts/Auth/AuthContext';
+import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Index = () => {
-
     const auth: any = useContext(AuthContext);
 
     const [visible, setVisible] = useState(false);
+
+    const navigate = useNavigate();
+
+    const handleNavigation = () => {
+        navigate('/handle-profile');
+    };
 
     return (
         <Container>
@@ -22,46 +28,44 @@ const Index = () => {
                 <MenuItems>
                     <Border />
                     <li>
-                        <a href="/">
-                            {/* TODO: Vai redirecionar para a pagina de alteração
-                                do perfil de usuário tipo empresa/candidato
-                            */}
-                            <img src={ProfileIcon} alt="perfil" />
-                            Meu perfil
-                        </a>
+                        <img onClick={handleNavigation} src={ProfileIcon} alt="perfil" />
+                        Meu perfil
                     </li>
-                    {(auth.user.type !== "USER") &&
-                        <li>
-                            <a href="insertjobs">
+                    {auth.user.type !== 'USER' && (
+                        // TODO: Corrigir quebra de layout da palavra vagas no mozilla
+                        <li className="jobs">
+                            <a className="jobs" href="insertjobs">
                                 <img src={NewJobIcon} alt="vagas" />
                                 Anunciar vagas
                             </a>
                         </li>
-                    }
+                    )}
                     <Border />
-                    <li onClick={() => { setVisible(!visible) }}>
+                    <li
+                        onClick={() => {
+                            setVisible(!visible);
+                        }}
+                    >
                         <img src={ConfigIcon} alt="configurações" />
                         <span>Configurações</span>
-                        {visible ? (<img src={UpArrowImage} alt="seta" />) :
-                            (<img src={DownArrowImage} alt="seta" />)
-                        }
+                        {visible ? (
+                            <img src={UpArrowImage} alt="seta" />
+                        ) : (
+                            <img src={DownArrowImage} alt="seta" />
+                        )}
                     </li>
-                    {visible &&
+                    {visible && (
                         <div>
                             <li>
-                                <a href="/">
-                                    <img src={PasswordIcon} alt="alterar senha" />
-                                    Alterar senha
-                                </a>
+                                <img src={PasswordIcon} alt="alterar senha" />
+                                Alterar senha
                             </li>
                         </div>
-                    }
+                    )}
                     <Border />
                     <li>
-                        <a href="/">
-                            <img src={TermsIcon} alt="termos" />
-                            Termo de uso e Privacidade
-                        </a>
+                        <img src={TermsIcon} alt="termos" />
+                        Termo de uso e Privacidade
                     </li>
                     <Border />
                     <li>
@@ -76,7 +80,7 @@ const Index = () => {
                 </MenuItems>
             </PopUp>
         </Container>
-    )
-}
+    );
+};
 
-export default Index
+export default Index;
