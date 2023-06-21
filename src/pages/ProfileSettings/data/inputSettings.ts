df89739b66f74404ce448d9ebdfbaf5f23a1c1d1
Igor Gonçalves <igordossantos38@gmail.com>
2023-06-21 18:26:29 -0300

    refactor: moving file to new structure folder

---
+++ src/pages/ProfileSettings/data/inputSettings.ts
@@ -0,0 +1,46 @@
+const inputConfigs = [
+    {
+        label: 'Email:',
+        type: 'email',
+        name: 'email',
+        placeholder: 'junior@soujunior.com',
+    },
+    {
+        label: 'CNPJ:',
+        type: 'text',
+        name: 'cnpj',
+        placeholder: 'Ex: xx.xxx.xxx/xxxx-xx',
+    },
+    {
+        label: 'Nome:',
+        type: 'text',
+        name: 'nome',
+        placeholder: 'Empresa SouJunior',
+    },
+    {
+        label: 'Site da empresa:',
+        type: 'text',
+        name: 'site',
+        placeholder: 'www.soujunior.com.br',
+    },
+    {
+        label: 'Outros:',
+        type: 'text',
+        name: 'redes',
+        placeholder: '@soujunior/instagram',
+    },
+    {
+        label: '',
+        type: 'text',
+        name: 'redes',
+        placeholder: '@soujunior/linkedin',
+    },
+    {
+        label: '',
+        type: 'text',
+        name: 'redes',
+        placeholder: '@soujunior/twitter',
+    },
+];
+
+export default inputConfigs;
