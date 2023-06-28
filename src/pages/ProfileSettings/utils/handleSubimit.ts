07e36a6899102f203938816fe16eb67d2f348091
Igor Gonçalves <igordossantos38@gmail.com>
2023-06-28 00:35:34 -0300

    refactor: create a file to handleSubmit function

---
+++ src/pages/ProfileSettings/utils/handleSubimit.ts
@@ -0,0 +1,34 @@
+export const handleSubmit = async (
+    e: any,
+    profileImg: any,
+    api: any,
+    auth: any,
+) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append('companyType', e.target.type.value);
+    formData.append('companySize', e.target.size.value);
+    formData.append('location', e.target.states.value);
+    formData.append('companySite', e.target.site.value);
+    formData.append('description', e.target.description.value);
+    formData.append('otherSite[instagram]', e.target.instagram.value);
+    formData.append('otherSite[linkedin]', e.target.linkedin.value);
+    formData.append('otherSite[twitter]', e.target.twitter.value);
+
+    if (profileImg) {
+        formData.append('file', profileImg);
+    }
+
+    formData.append(
+        'profileKey', auth.user.profileKey ?? 'lkjhgfdsa',
+    );
+
+    try {
+        const res = await api.updateCompanyProfile(formData);
+        return res;
+        //TODO mensagem de envio com sucesso / pop-up "atualizações salvas"
+    } catch (error) {
+        //TODO ver mensagem de erro para o usuário
+    }
+};
