239b8b61faf1ff2ff75789791d9c56030458f1cd
FilipeLeoni <filipe_sempre@outlook.com>
2023-07-05 14:50:41 -0300

    fix: code corrections

--- src/components/FeedVagas/QuickFilter/styles.ts
+++ src/components/FeedVagas/QuickFilter/styles.ts
@@ -1,14 +1,15 @@
 import styled from "styled-components";
+import Teste from "../../../assets/imgs/arrowDown.svg";
 
 export const Select = styled.select`
     width: 100%;
     padding: 2px 18px;
     border: 1px solid #C1DFFF;
     border-radius: 100px;
-    -webkit-appearance: none; /* Remove a aparência padrão da seta */
+    -webkit-appearance: none;
     -moz-appearance: none;
     appearance: none;
-    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='4' viewBox='0 0 8 4' fill='%23515050'%3E%3Cpath d='M0.666504 0L3.99984 3.33333L7.33317 0H0.666504Z'/%3E%3C/svg%3E"); /* Define o código SVG para a seta */
+    background-image: url(${Teste}); 
     background-position: calc(100% - 16px) center; 
     background-repeat: no-repeat; 
     background-size: 8px; 
