49e25148f6ca4d0499f0bb6c421b244577263ff9
FilipeLeoni <filipe_sempre@outlook.com>
2023-07-14 18:00:52 -0300

    style: Footer changed in home page

--- src/pages/styles/Home.styles.ts
+++ src/pages/styles/Home.styles.ts
@@ -491,39 +491,3 @@ export const GooglePlayButton = styled.img`
         width: 200px;
     }
 `;
-
-export const Position = styled.div`
-    margin-top: auto;
-`
-export const Row = styled.div`  
-    width: 90%;
-    border-top: 2px solid #979797;
-    opacity: 0.2;
-    margin-bottom: auto;
-`
-export const Copyright = styled.span`
-    display: flex;
-    justify-content: center;
-    font-size: 14px;
-    opacity: 0.65;
-    padding-bottom: 20px;
-`
-
-export const MainFooter = styled.main`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    padding-left: 10px;
-    padding-right: 10px;
-    padding-top: 60px;
-    padding-bottom: 20px;
-
-    h1 {
-        font-size: 35px;
-        font-weight: 700;
-        color: #046AD0;
-        padding-top: 80px;
-    }
-`
