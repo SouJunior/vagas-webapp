ee87b32a64dbf05afded1c4bb4bfd22bba21415a
Bruno Vinícius <94939630+brunowzz@users.noreply.github.com>
2024-02-20 19:26:57 -0300

    feat: added new container components for carousel pages

--- src/pages/CarouselAreas/RenderArea.tsx
+++ src/pages/CarouselAreas/RenderArea.tsx
@@ -1,27 +1,25 @@
-import { useParams } from "react-router-dom"
-import Product from "./Product";
-import Agile from "./Agile";
+import { useParams } from 'react-router-dom';
+import Product from './Product';
+import Agile from './Agile';
+import UxUi from './UxUi';
 
 const RenderArea: React.FC = () => {
-
     let { areaId = 'default' } = useParams<{ areaId: string }>();
 
     const getArea = (areaId: string) => {
         switch (areaId) {
             case 'product':
-                return <Product/>
+                return <Product />;
             case 'agile':
-                return <Agile/>
+                return <Agile />;
+            case 'uxuidesign':
+                return <UxUi />;
             default:
-                return <div>Seção não encontrada</div>
+                return <div>Seção não encontrada</div>;
         }
-    }
+    };
 
-    return (
-        <>
-            {getArea(areaId)}
-        </>
-    )
-}
+    return <>{getArea(areaId)}</>;
+};
 
-export default RenderArea
+export default RenderArea;
