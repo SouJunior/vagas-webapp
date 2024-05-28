b516430963de70095b1626f9e22d40407e0b1db5
Bruno Vinícius Barreiras de Oliveira <94939630+brunowzz@users.noreply.github.com>
2024-05-28 16:15:06 -0300

    refactor: add subtitle sm component

---
+++ src/components/Ui/SubtitleSm/style.ts
@@ -0,0 +1,13 @@
+import { SubtitleSmProps } from './types';
+
+import styled from 'styled-components';
+
+export const SubtitleSm = styled.h2<SubtitleSmProps>`
+    font-family: 'Radio Canada';
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 120%;
+    color: #003986;
+    text-transform: ${({ transform }) => transform || 'none'};
+`;
