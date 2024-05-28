5be449829da822714ad220c59fb47f547d65a9a2
Bruno Vinícius Barreiras de Oliveira <94939630+brunowzz@users.noreply.github.com>
2024-05-28 17:08:04 -0300

    refactor: add badge component

---
+++ src/components/Ui/Badge/style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Badge = styled.span`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 4px 16px;
+    border-radius: 500px;
+    background: rgba(51, 138, 255, 0.5);
+`;
