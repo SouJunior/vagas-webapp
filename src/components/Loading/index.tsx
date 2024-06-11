d23de16acdf29fc4c1247892bccf709316e6ffe2
Bruno Vinícius Barreiras de Oliveira <94939630+brunowzz@users.noreply.github.com>
2024-06-11 13:01:28 -0300

    feat: add loading

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './style';
+
+const Loading = () => {
+    return (
+        <S.LoadingContainer>
+            <S.Spinner />
+        </S.LoadingContainer>
+    );
+};
+
+export default Loading;
