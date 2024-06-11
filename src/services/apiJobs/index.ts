fc132137293366ca6a55ece414eaf8e432afc1e3
Bruno Vinícius Barreiras de Oliveira <94939630+brunowzz@users.noreply.github.com>
2024-06-11 18:09:59 -0300

    feat: add design to header in feed jobs

--- src/services/apiJobs/index.ts
+++ src/services/apiJobs/index.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const apiJobs = axios.create({
-    baseURL: process.env.REACT_APP_URL_JOBS || 'http://localhost:3333',
+    baseURL:
+        process.env.REACT_APP_URL_JOBS_LOCAL || process.env.REACT_APP_URL_JOBS,
 });
 
 export default apiJobs;
