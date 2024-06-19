b556fdd4ec4bb34317d9227e3c887f59f20a8560
Wouerner Brandão <wouerner@soujunior.tech>
2024-06-19 16:54:39 -0300

    Update index.ts

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: process.env.REACT_APP_URL,
-    baseURL: 'https://vagas-backend.soujunior.tech',
+    baseURL: process.env.REACT_APP_URL,
+    // baseURL: 'https://vagas-backend.soujunior.tech',
 });
 
 export default api;
