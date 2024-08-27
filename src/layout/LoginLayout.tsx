f49c6dd8a11fc8ac5822a0fdb763333c6d6ae569
Joao Gabriel Marinho <jgabriel.marinho.dev@hotmail.com>
2024-08-26 21:39:21 -0300

    Fix: Adiciona o rodapé da SouJunior na tela de login

---
+++ src/layout/LoginLayout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom';
+import FooterDefault from '../components/FooterDefault';
+
+const LoginLayout = () => {
+    return (
+        <>
+            <Outlet />
+            <FooterDefault />
+        </>
+    );
+};
+
+export default LoginLayout;
