522e0a4c3d7369ffdced7e6d6aa3c7ada5355d88
Vanilo Ferreira <vanilo.ferreira03@gmail.com>
2024-11-12 21:21:41 -0300

    feat: Call Alert component in Modal component

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -11,10 +11,10 @@ import {
     Close
 } from './styles';
 
-import { Box, Alert, AlertTitle, Collapse } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 
 import Confirme from '../../assets/imgs/confirme.svg';
+import AlertNotifications from '../Alert';
 
 interface IModal {
     isOpen: boolean;
@@ -25,18 +25,18 @@ interface IModal {
 
 export default function Modal({ isOpen, setOpen, isApplicationApplied, setApplicationApplied }: IModal) {
 
-    const [confirmationAlert, setConfirmationAlert] = useState(false);
-    const [denialAlert, setDenialAlert] = useState(false);
+    const [error, setError] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
     const [confirmationModal, setConfirmationModal] = useState(false);
 
     function cancelApplication() {
-        setDenialAlert(true);
+        setShowAlert(true);
         setOpen(!isOpen);
         setApplicationApplied(false);
     }
 
     function ConfirmApplication() {
-        setConfirmationAlert(true);
+        setShowAlert(true);
         setConfirmationModal(true);
         setApplicationApplied(true);
     }
@@ -49,7 +49,6 @@ export default function Modal({ isOpen, setOpen, isApplicationApplied, setApplic
     if (isOpen) {
         return (
             <Background>
-
 
                 <ContainerModal>
                     <Close>
@@ -78,81 +77,39 @@ export default function Modal({ isOpen, setOpen, isApplicationApplied, setApplic
         )
     } else if (!isOpen && isApplicationApplied) {
         return (
-            <Box sx={{ minWidth: "635px", position: "fixed", top: "1%", left: "56%" }}>
-                <Collapse in={confirmationAlert}>
-                    <Box sx={{
-                        marginTop: "2px",
-                        display: "flex",
-                        minWidth: "100%"
-                    }}>
-                        <Alert severity="success" sx={{
-                            display: "flex",
-                            minWidth: "100%",
-                            justifyContent: "flex-start",
-                            marginLeft: "2px",
-                            marginRight: "30px",
-                            color: "#083D07",
-                            backgroundColor: "#C1F7C1",
-                            fontFamily: 'Radio Canada',
-                            fontSize: "16px",
-                            fontWeight: "400",
-                            lineHeight: "22.4px"
-                        }}>
-                            <AlertTitle sx={{ fontFamily: 'Radio Canada', fontSize: "20px", fontWeight: "500", lineHeight: "24px" }}>Sucesso</AlertTitle>
-                            Sua aplicação para a candidatura foi registrada.
-                        </Alert>
-                    </Box>
-                    <CloseIcon onClick={() => setConfirmationAlert(false)} fontSize="inherit"
-                        sx={{
-                            width: "24px",
-                            height: "24px",
-                            marginLeft: "50px",
-                            position: "relative",
-                            bottom: "50%",
-                            left: "88%",
-                            color: "#083D07"
-                        }} />
-                </Collapse>
-            </Box>
+            <AlertNotifications
+                isShowAlert={showAlert}
+                setShowAlert={setShowAlert}
+                titleAlert="Sucesso"
+                textAlert="Sua aplicação para a candidatura foi registrada."
+                textColor="#083D07"
+                backgroundColor="#C1F7C1"
+                alertSeverityType="success"
+            />
+        )
+    } else if (error) {
+        return (
+            <AlertNotifications
+                isShowAlert={showAlert}
+                setShowAlert={setShowAlert}
+                titleAlert="Erro"
+                textAlert="Seu registro de candidatura falhou. Tente novamente."
+                textColor="#54010E"
+                backgroundColor="#F6BEBE"
+                alertSeverityType="error"
+            />
         )
     } else {
         return (
-            <Box sx={{ minWidth: "635px", minHeight: "62px", position: "fixed", top: "1%", left: "56%" }}>
-                <Collapse in={denialAlert}>
-                    <Box sx={{
-                        marginTop: "2px",
-                        display: "flex",
-                        minWidth: "100%"
-                    }}>
-                        <Alert severity="success" sx={{
-                            display: "flex",
-                            minWidth: "100%",
-                            justifyContent: "flex-start",
-                            marginLeft: "2px",
-                            marginRight: "30px",
-                            color: "#083D07",
-                            backgroundColor: "#C1F7C1",
-                            fontFamily: 'Radio Canada',
-                            fontSize: "16px",
-                            fontWeight: "400",
-                            lineHeight: "22.4px"
-                        }}>
-                            <AlertTitle sx={{ fontFamily: 'Radio Canada', fontSize: "20px", fontWeight: "500", lineHeight: "24px" }}>Sucesso</AlertTitle>
-                            Sua aplicação para a candidatura não foi registrada.
-                        </Alert>
-                    </Box>
-                    <CloseIcon onClick={() => setDenialAlert(false)} fontSize="inherit"
-                        sx={{
-                            width: "24px",
-                            height: "24px",
-                            marginLeft: "50px",
-                            position: "relative",
-                            bottom: "50%",
-                            left: "88%",
-                            color: "#083D07"
-                        }} />
-                </Collapse>
-            </Box>
+            <AlertNotifications
+                isShowAlert={showAlert}
+                setShowAlert={setShowAlert}
+                titleAlert="Candidatura não realizada"
+                textAlert="Sua aplicação para a candidatura não foi registrada."
+                textColor="#344054"
+                backgroundColor="#F2F4F7"
+                alertSeverityType="success"
+            />
         )
     }
 }
