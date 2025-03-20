cd7b899b71daaac7eea7672a99b335f28a40b309
Thales Menezes <thlmenezes.dev@gmail.com>
2025-03-19 21:49:47 -0300

    feat: integrate vite-tsconfig-paths for improved TypeScript path resolution and update build script

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  base: '/',
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    outDir: 'build',
+    assetsDir: 'assets',
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  },
+  server: {
+    port: 3000
+  }
+});
+
