f0e6bc6895798572b25f7eea667b4e4f31d9074f
Thales Menezes <thlmenezes.dev@gmail.com>
2025-03-19 22:02:08 -0300

    chore: remove vite-tsconfig-paths

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import commonjs from 'vite-plugin-commonjs';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/',
-  plugins: [react(), tsconfigPaths(), commonjs()],
+  plugins: [commonjs(), react()],
   build: {
     outDir: 'build',
     assetsDir: 'assets',
