a202ec4c44c9100628f518f9e128a167587d410a
Thales Menezes <thlmenezes.dev@gmail.com>
2025-03-19 22:03:38 -0300

    chore: apply prettier

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,19 @@ import commonjs from 'vite-plugin-commonjs';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
-  plugins: [commonjs(), react()],
-  build: {
-    outDir: 'build',
-    assetsDir: 'assets',
-    sourcemap: true,
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  },
-  server: {
-    port: 3000
-  }
+    base: '/',
+    plugins: [commonjs(), react()],
+    build: {
+        outDir: 'build',
+        assetsDir: 'assets',
+        sourcemap: true,
+        rollupOptions: {
+            input: {
+                main: './index.html',
+            },
+        },
+    },
+    server: {
+        port: 3000,
+    },
 });
-
