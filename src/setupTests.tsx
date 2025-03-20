9ec87818b96bc499390a85405a570bc5975a28e5
Thales Menezes <thlmenezes.dev@gmail.com>
2025-03-19 22:39:48 -0300

    test: enhance App component tests with additional providers and mock implementations

--- src/setupTests.tsx
+++ src/setupTests.tsx
@@ -2,17 +2,14 @@ import '@testing-library/jest-dom';
 import { ThemeProvider } from 'styled-components';
 import { mainTheme } from './components/styles/Theme';
 import { render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
 import { ReactElement } from 'react';
 
 const customRender = (ui: ReactElement, options = {}) =>
   render(ui, {
     wrapper: ({ children }) => (
-      <BrowserRouter>
-        <ThemeProvider theme={mainTheme}>
-          {children}
-        </ThemeProvider>
-      </BrowserRouter>
+      <ThemeProvider theme={mainTheme}>
+        {children}
+      </ThemeProvider>
     ),
     ...options,
   });
