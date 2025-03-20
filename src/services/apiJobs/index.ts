1e5a8046a75f2d5701ce01ad1da9c4e6133b31a6
Thales Menezes <thlmenezes.dev@gmail.com>
2025-03-19 21:50:06 -0300

    refactor: update environment variable names to Vite convention in configuration and code

--- src/services/apiJobs/index.ts
+++ src/services/apiJobs/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apiJobs = axios.create({
     baseURL:
-        import.meta.env.REACT_APP_URL_JOBS_LOCAL || import.meta.env.REACT_APP_URL_JOBS,
+        import.meta.env.VITE_APP_URL_JOBS_LOCAL || import.meta.env.VITE_APP_URL_JOBS,
 });
 
 export default apiJobs;
