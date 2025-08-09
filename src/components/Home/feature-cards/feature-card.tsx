e75a93c92e012c5595b7923f8a091a79c006ea3c
Leidejane <leidejanedarosa.81@gmail.com>
2025-08-09 16:36:29 -0300

    feat:responsiveness

--- src/components/Home/feature-cards/feature-card.tsx
+++ src/components/Home/feature-cards/feature-card.tsx
@@ -12,41 +12,37 @@ export const FeatureCard: React.FC<FeatureCardProps> = ({
     imgAlt,
 }) => {
     return (
-        <section
-            className="w-full max-w-[1440px] min-w-96 px-8 py-6 flex flex-wrap justify-start items-stretch gap-6"
+        <div
+            className="flex items-center justify-center w-[350px] py-1.5 px-4 bg-white rounded-[10px] shadow-[0px_2px_4px_0px_rgba(0,0,0,0.10)] flex-col md:max-w-[365px] md:h-32 lg:min-w-[443px]"
             aria-labelledby="section-benefits"
         >
-            <h2 id="section-benefits" className="sr-only">
-                Leidejane
-            </h2>
-
             <ul className="flex flex-wrap gap-6">
                 <li
-                    className="w-96 bg-white rounded-[10px] shadow-md flex flex-col p-4"
+                    className="flex flex-col"
                     role="article"
                     aria-labelledby="card-why"
                 >
-                    <figure className="flex gap-4 items-center">
+                    <figure className="flex items-center h-auto">
                         <img
-                            className="w-32 h-28 object-contain"
+                            className="w-[126px] md:w-24 lg:w-[126px]"
                             src={imgSrc}
                             alt={imgAlt}
                             loading="lazy"
                         />
-                        <figcaption className="flex-1 flex flex-col gap-1">
+                        <figcaption className="flex flex-col gap-1 ">
                             <h3
                                 id="card-why"
-                                className="text-blue-900 text-xl font-medium leading-normal"
+                                className="text-blue-900 text-base font-medium leading-tight"
                             >
                                 {title}
                             </h3>
-                            <p className="text-blue-900 text-base leading-snug">
+                            <p className="text-blue-900 text-sm font-normal leading-tight">
                                 {description}
                             </p>
                         </figcaption>
                     </figure>
                 </li>
             </ul>
-        </section>
+        </div>
     );
 };
--- src/components/Home/feature-cards/index.tsx
+++ src/components/Home/feature-cards/index.tsx
@@ -3,10 +3,10 @@ import { featureCardsData } from './feature-cards-data';
 
 export const FeatureCards: React.FC = ({}) => {
     return (
-        <>
+        <section className="w-full max-w-[1440px] flex flex-col p-4 gap-6 items-center md:flex-row md:flex-wrap md:justify-center md:gap-x-4 md:gap-y-6 lg:gap-6 lg:flex-nowrap">
             {featureCardsData.map((card, index) => (
                 <FeatureCard key={index} {...card} />
             ))}
-        </>
+        </section>
     );
 };
