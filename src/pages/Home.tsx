9b7c4b7c80b778091d5ff2360d9698e314922938
Gui Monteiro <guilhermemonteiroro@gmail.com>
2025-09-09 16:22:21 -0300

    refactor: paste name

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ import { CareerTransitionSection } from '../components/Home/career-transition-se
 
 import TestimonialSection from '../components/Home/testimonial-section/index';
 import { EnterprisesSection } from '../components/Home/enterprises-section';
-import { AnimationEntry } from '../components/Ui/animation-entry';
+import { AnimationEntry } from '../components/Ui/animation-entry/index';
 
 export interface AreaProps {
     id: string;
