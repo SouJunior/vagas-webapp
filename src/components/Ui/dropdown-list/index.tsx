c2f8c43e4dc49c3c5949ef56a9e2cb5fe7693839
Leidejane <leidejanedarosa.81@gmail.com>
2025-10-04 11:30:33 -0300

    feat: simplify DropdownList API using Object.assign

--- src/components/Ui/dropdown-list/index.tsx
+++ src/components/Ui/dropdown-list/index.tsx
@@ -4,13 +4,12 @@ import { DropdownListPopover } from './DropdownListPopover';
 import { DropdownListRoot } from './DropdownListRoot';
 import { DropdownListTrigger } from './DropdownListTrigger';
 
-export const DropdownList = {
-  Root: DropdownListRoot,
+export const DropdownList = Object.assign(DropdownListRoot, {
   Popover: DropdownListPopover,
   Trigger: DropdownListTrigger,
   Content: DropdownListContent,
   Item: DropdownListItem,
-};
+});
 
 export type {
   DropdownListContentProps,
