62be01357eb8ecc23cb0b08d298fc1949e228a37
Leidejane <leidejanedarosa.81@gmail.com>
2025-10-14 21:42:16 -0300

    refactor: update header routing to use loggedInUser variant

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import type { HeaderProps } from './types';
 import { CompaniesHeader } from './variants/CompaniesHeader';
 import { DefaultHeader } from './variants/DefaultHeader/DefaultHeader';
+import { LoggedInUserHeader } from './variants/LoggedInUserHeader/LoggedInUserHeader';
 import { NavigationHeader } from './variants/NavigationHeader';
-import { PortalCompanyHeader } from './variants/PortalCompanyHeader/PortalCompanyHeader';
 
 const Header: React.FC<HeaderProps> = (props) => {
   switch (props.variant) {
@@ -11,8 +11,8 @@ const Header: React.FC<HeaderProps> = (props) => {
       return <DefaultHeader {...props} />;
     case 'companies':
       return <CompaniesHeader {...props} />;
-    case 'portalCompany':
-      return <PortalCompanyHeader {...props} />;
+    case 'loggedInUser':
+      return <LoggedInUserHeader {...props} />;
     case 'navigation':
       return <NavigationHeader {...props} />;
     default:
